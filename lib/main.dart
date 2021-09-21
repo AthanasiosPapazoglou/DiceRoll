c8ccae21dc8fd545eec63699fe4504968e29144b
Athanasios Papapzoglou <athanasiospap95@gmail.com>
2021-09-21 19:01:45 +0300

    actionListener/Buttons/RNG Numbers Implemented

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.green[700],
         appBar: AppBar(
-          title: Text('DiceRoll'),
-          backgroundColor: Colors.red,
+          title: Text('DiceRoll by Nasos'),
+          backgroundColor: Colors.blueGrey[800],
         ),
         body: DicePage(),
       ),
@@ -15,24 +17,48 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNo = 1;
+  int rightDiceNo = 1;
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Row(
         children: <Widget>[
           Expanded(
-            child: Padding(
-              padding: EdgeInsets.all(12),
-            child: Image.asset('images/dice1.png'),
+            child: FlatButton(
+              onPressed: () {
+                setState(
+                  () {
+                    Random random = new Random();
+                    leftDiceNo = random.nextInt(6) + 1;
+                    rightDiceNo = random.nextInt(6) + 1;
+                  },
+                );
+              },
+              child: Image.asset('images/dice$leftDiceNo.png'),
             ),
           ),
           Expanded(
-            child: Padding(
-              padding: EdgeInsets.all(12),
-            child: Image.asset('images/dice6.png'),
+            child: FlatButton(
+              onPressed: () {
+                setState(
+                  () {
+                    Random random = new Random();
+                    leftDiceNo = random.nextInt(6) + 1;
+                    rightDiceNo = random.nextInt(6) + 1;
+                  },
+                );
+              },
+              child: Image.asset('images/dice$rightDiceNo.png'),
+            ),
           ),
-        ),
         ],
       ),
     );
